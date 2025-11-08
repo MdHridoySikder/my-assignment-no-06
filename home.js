48a32e194e25c39c0957550a447f205e8f2929d9
Md Hridoy Sikder <hridoy702345@gmail.com>
2025-11-08 09:48:00 +0600

    assignment done

--- home.js
+++ home.js
@@ -5,6 +5,17 @@ const getCategories = () => {
         .then(data => createCategories(data.categories))
 
 }
+const spinnerManager = (status) => {
+    if (status === true) {
+        document.getElementById("spinner").classList.remove("hidden")
+        document.getElementById("plantSector").classList.add("hidden")
+    }
+    else {
+        document.getElementById("plantSector").classList.remove("hidden")
+        document.getElementById("spinner").classList.add("hidden")
+
+    }
+}
 
 const removerbtn = () => {
     const removeing = document.querySelectorAll(".removerbtn")
@@ -13,34 +24,38 @@ const removerbtn = () => {
     }
 }
 const loadData = (id) => {
-    const url = (https://openapi.programming-hero.com/api/category/${id});
+    spinnerManager(true)
+    const url = (`https://openapi.programming-hero.com/api/category/${id}`);
     fetch(url)
         .then(res => res.json())
         .then(data => descriptions(data.plants))
     removefromall();
     removerbtn();
-    const hoverEffect = document.getElementById(btn-${id});
+    const hoverEffect = document.getElementById(`btn-${id}`);
     hoverEffect.classList.add("active")
 
 
 }
 
 const descriptions = (details) => {
+
     const desId = document.getElementById("plantSector");
     desId.innerHTML = "";
     for (let detail of details) {
         const makingCard = document.createElement("div");
         makingCard.innerHTML = `<div class="p-4 bg-white rounded-xl h-full w-63 space-y-2">
-          <img class="w-[311px] h-[186px] object-cover rounded-lg mx-auto " src="${detail.image}" alt="">
-          <h1 onclick="loadPlantDetails(${detail.id})" class="font-semibold">${detail.name}</h1>
-          <p class="text-[#1F293780] text-[12px]">${detail.description}</p>
-          <div class="flex justify-between items-center">
-            <div class="text-[#15803D] bg-[#DCFCE7] font-semibold text-sm my-2 p-2 rounded-full">${detail.category}</div>
-            <div class="font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${detail.price}</div>
-          </div>
-          <button class="btn bg-[#15803D] text-white text-sm font-semibold rounded-full w-full hover:scale-105 shadow-sm">Add to Cart</button>
-        </div> `;
+            <img class="w-[311px] h-[186px] object-cover rounded-lg mx-auto " src="${detail.image}" alt="">
+            <h1 onclick="loadPlantDetail(${detail.id})" class="font-semibold hover:cursor-pointer">${detail.name}</h1>
+            <p class="text-[#1F293780] text-[12px]">${detail.description}</p>
+            <div class="flex justify-between items-center">
+                <div class="text-[#15803D] bg-[#DCFCE7] font-semibold text-sm my-2 p-2 rounded-full">${detail.category}</div>
+                <div class="font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${detail.price}</div>
+            </div>
+            <button onclick="addCart(${detail.id})" class="btn bg-[#15803D] text-white text-sm font-semibold rounded-full w-full hover:scale-105 shadow-sm">Add to Cart</button>
+            </div> `;
         desId.appendChild(makingCard)
+
+        spinnerManager(false)
     }
 
 }
@@ -68,6 +83,7 @@ const addtoallbtn = () => {
 
 
 const showAll = () => {
+    spinnerManager(true)
     const url = ("https://openapi.programming-hero.com/api/plants")
     fetch(url)
         .then(res => res.json())
@@ -76,49 +92,104 @@ const showAll = () => {
 
 }
 const loadAll = (all) => {
+
     const desId = document.getElementById("plantSector");
     desId.innerHTML = "";
     for (let info of all) {
         const makingCard = document.createElement("div");
         makingCard.innerHTML = `<div class="p-4 bg-white rounded-xl h-full w-63 space-y-2 shadow-md">
-          <img class="w-[311px] h-[186px] object-cover rounded-lg mx-auto " src="${info.image}" alt="">
-          <h1 onclick="loadPlantDetail(${info.id})" class="font-semibold">${info.name}</h1>
-          <p class="text-[#1F293780] text-[12px]">${info.description}</p>
-          <div class="flex justify-between items-center">
-            <div class="text-[#15803D] bg-[#DCFCE7] font-semibold text-sm my-2 p-2 rounded-full">${info.category}</div>
-            <div class="font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${info.price}</div>
-          </div>
-          <button class="btn bg-[#15803D] text-white text-sm font-semibold rounded-full w-full hover:scale-105 shadow-sm">Add to Cart</button>
-        </div> `
+            <img class="w-[311px] h-[186px] object-cover rounded-lg mx-auto " src="${info.image}" alt="">
+            <h1 onclick="loadPlantDetail(${info.id})" class="font-semibold hover:cursor-pointer">${info.name}</h1>
+            <p class="text-[#1F293780] text-[12px]">${info.description}</p>
+            <div class="flex justify-between items-center">
+                <div class="text-[#15803D] bg-[#DCFCE7] font-semibold text-sm my-2 p-2 rounded-full">${info.category}</div>
+                <div class="font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign"></i>${info.price}</div>
+            </div>
+            <button onclick="addCart(${info.id})" class="btn bg-[#15803D] text-white text-sm font-semibold rounded-full w-full hover:scale-105 shadow-sm">Add to Cart</button>
+            </div> `
         desId.appendChild(makingCard)
 
         addtoallbtn()
-
+        spinnerManager(false)
     }
 
 }
 
+let cartTotal = 0;
+
+
+
+const addCart = (id) => {
+    const url = (`https://openapi.programming-hero.com/api/plant/${id}`)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => addToCard(data.plants));
+}
+
+const addToCard = (cart) => {
+    const cartName = document.getElementById("cart-container")
+
+
+    const newCart = document.createElement("div");
+    newCart.innerHTML = `<div id="add-Cart-${cart.id}" class="flex justify-around bg-green-50 rounded-lg ">
+                <div>
+                <h1 class="font-semibold">${cart.name}</h1>
+                <p id="price">${cart.price}</p>
+                </div>
+                <div class= "hover:scale-115" onclick="removeCart(${cart.id})"><i class="fa-solid fa-xmark" style="color: #eb0000;"></i></div>
+            </div>`
+
+    cartName.appendChild(newCart);
+
+    cartTotal += Number(cart.price);
+    document.getElementById("cart-total").innerText = cartTotal;
+}
+
+
+
+const removeCart = (id) => {
+    const getId = document.getElementById(`add-Cart-${id}`);
+    getId.remove();
+
+
+
+    minusTheCart(id)
+}
+
+const minusTheCart = (id) => {
+    const url = (`https://openapi.programming-hero.com/api/plant/${id}`)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => minus(data.plants))
+}
+
+
+const minus = (plants) => {
+    cartTotal -= Number(plants.price);
+    document.getElementById("cart-total").innerText = cartTotal;
+}
+
+
 const loadPlantDetail = async (id) => {
-    const url = (https://openapi.programming-hero.com/api/plant/${id})
+    const url = (`https://openapi.programming-hero.com/api/plant/${id}`)
     const res = await fetch(url)
     const details = await res.json();
     displayModal(details.plants)
-    }
+}
 
-const displayModal = (word) =>{
+const displayModal = (word) => {
     console.log(word);
     const modelID = document.getElementById("modelHold");
     modelID.innerHTML = ` <h1 class="font-bold text-lg">${word.name}</h1>
-      <img class="w-[311px] h-[186px] object-cover rounded-lg mx-auto " src="${word.image}" alt="">
-      <p><span class="font-bold text-md">Category: </span>${word.category}</p>
-      <p><span class="font-bold text-md">Price:<i class="fa-solid fa-bangladeshi-taka-sign"></i></span>${word.price}</p>
-      <p><span class="font-bold text-sm">Description: </span>${word.description}</p>`
-   document.getElementById("my_modal_1").showModal();
-    
-    
-    
-    
+        <img class="w-[311px] h-[186px] object-cover rounded-lg mx-auto " src="${word.image}" alt="">
+        <p><span class="font-bold text-md">Category: </span>${word.category}</p>
+        <p><span class="font-bold text-md">Price:<i class="fa-solid fa-bangladeshi-taka-sign"></i></span>${word.price}</p>
+        <p><span class="font-bold text-sm">Description: </span>${word.description}</p>`
+    document.getElementById("my_modal_1").showModal();
+
 }
+
+
 
 showAll();
 getCategories();
